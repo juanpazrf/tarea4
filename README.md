2c7c5e54fc5fbeff72c63d1d3fb596fd1a744ea7
juan <juan.paz@gateway.com.pe>
2021-03-29 16:51:31 -0500

    Modificacion Vanilla CSS, utilizando CSS Bulma

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# tarea4
-Tarea4
+# Tarea_Curso
---
+++ index.js
@@ -0,0 +1,8 @@
+var openCloseMenu = () =>  {
+    var _menu = document.getElementById('Menu');
+    if (_menu.style.display === "none") {
+        _menu.style.display = "block";
+    } else {
+        _menu.style.display = "none";
+    }
+}
